4cbc42f8e2ef5e8c3639c0deacbaa0b6bde88e8a
Marina Fernández Pastor <marina.fdezpas@gmail.com>
2021-01-28 10:50:17 +0100

    add first vet functions of main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 'use strict'
 
+//Se genera un número aleatorio y se muestra en consola
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
   }
+  console.log(getRandomNumber(100));
+
+//Me traigo el intento de la usuaria
+const attemptNumber = document.querySelector('.js-attempt')
+
+const tryButton = document.querySelector('.js-button');
+function showClue() {}
+
+//Condicional 
+const clueElement = document.querySelector('.js-clue');
+if (attemptNumber > getRandomNumber()) {
+  clueElement.innerHTML = 'Demasiado alto';
+} else if (attemptNumber < getRandomNumber()) {
+  clueElement.innerHTML = 'Demasiado bajo';
+} else if (attemptNumber === getRandomNumber()) {
+  clueElement.innerHTML = '¡Acertaste, campeona!';
+} else {
+  clueElement.innerHTML = 'El número debe estar entre 1 y 100';}
